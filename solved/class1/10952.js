3b1083312208b3c07a939a7a234e884c14b384f1
Hyesu Son <46313348+sduu@users.noreply.github.com>
2022-05-03 14:28:39 +0900

    class1 / 10952 문제 추가

---
+++ solved/class1/10952.js
@@ -0,0 +1,10 @@
+/* https://www.acmicpc.net/problem/10952 */
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let answer = '';
+
+for (let i = 0; i < input.length - 1; i++) {
+  const array = input[i].split(' ');
+  answer += `${+array[0] + +array[1]} \n`;
+}
+
+console.log(answer);
