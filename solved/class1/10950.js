9074442555d0cbf93aa72e10f7edeeeba3b775b4
Hyesu Son <46313348+sduu@users.noreply.github.com>
2022-05-03 14:07:20 +0900

    class1 / 10950 문제 추가

---
+++ solved/class1/10950.js
@@ -0,0 +1,8 @@
+/* https://www.acmicpc.net/problem/10950 */
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const num = input[0];
+
+for (let i = 1; i <= num; i++) {
+  const num = input[i].split(' ').map(Number);
+  console.log(num[0] + num[1]);
+}
