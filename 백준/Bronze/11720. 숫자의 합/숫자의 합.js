fb7013d07d2e031f8d9abf8e44e5c8ac585e2308
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-02 16:25:48 +0900

    [Bronze IV] Title: 숫자의 합, Time: 128 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const array = [...input[1]].map(Number);
+let answer = 0;
+
+for (let i = 0; i < input[0]; i++) {
+	answer += array[i];
+}
+
+console.log(answer);
