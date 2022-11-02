fd5591a34cad95d9596317a37484df81c3695bf7
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-02 16:25:25 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+input.forEach(v => {
+  const array = v.split(' ');
+  console.log(+array[0] + +array[1]);
+});
