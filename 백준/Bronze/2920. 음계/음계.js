9b9aa1771811405f4485b2b1d8f17c2bdf8fd917
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-02 16:24:42 +0900

    [Bronze II] Title: 음계, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const array = input.slice();
+
+if (array.sort((a, b) => a - b).join(' ') === input.join(' ')) {
+	console.log('ascending');
+} else if (array.sort((a, b) => b - a).join(' ') === input.join(' ')) {
+	console.log('descending');
+} else {
+	console.log('mixed');
+}
