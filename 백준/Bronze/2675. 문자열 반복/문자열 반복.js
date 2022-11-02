ad20fdbe9d2bb133aee76d4e37f0f766edc65812
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-02 16:24:25 +0900

    [Bronze II] Title: 문자열 반복, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let answer = '';
+
+for (let i = 0; i < input.length; i++) {
+  const [num, [...txt] = ''] = input[i].trim().split(' ');
+
+  if (txt.length > 0) {
+    for (let j = 0; j < txt.length; j++) {
+      answer += txt[j].repeat(num);
+    }
+    answer += '\n';
+  }
+}
+
+console.log(answer);
