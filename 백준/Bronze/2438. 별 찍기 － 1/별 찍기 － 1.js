93c923e68627d6a097f63a3368e883230e82a242
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-02 16:23:57 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 120 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('/dev/stdin');
+const a = Number(input);
+let answer = '';
+
+for (let i = 1; i <= a; i++) {
+  for (let j = 0; j < i; j++) {
+    answer += '*';
+  }
+  answer += '\n';
+}
+
+console.log(answer);
