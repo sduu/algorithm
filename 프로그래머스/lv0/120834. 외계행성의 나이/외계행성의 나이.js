29fd6fd14b7bc034066b6f44137a6f75e9e69e8b
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-04 17:24:38 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,4 @@
+function solution(age) {
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    return [...age.toString()].map(v => alphabet[v]).join("");
+}
