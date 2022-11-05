5abca5225c30c0f4c4368bad5b1fcefc868b9b61
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-05 23:04:17 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = 0;
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                answer++;
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
