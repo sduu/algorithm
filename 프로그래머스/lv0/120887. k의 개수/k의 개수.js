3bf0075846cadd7dad3ac3b97f785b04d89f6050
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-06 19:47:08 +0900

    [level 0] Title: k의 개수, Time: 3.27 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,3 +1,3 @@
 function solution(i, j, k) {
-    return new Array(j - i + 1).fill().map((v, idx) => idx + i).join("").match(new RegExp(k, "g"))?.length || 0;
+    return new Array(j - i + 1).fill().map((v, idx) => idx + i).join("").split(k).length - 1;
 }
