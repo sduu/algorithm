edb8fe75523314a0df3f5233af176fca222c5084
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-29 10:21:49 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,8 +1,1 @@
-function solution(A, B) {
-    const a = [...A];
-    for (let i = 0; i < A.length; i++) {
-        if (a.join('') === B) return i;
-        a.unshift(a.pop());
-    }
-    return -1;
-}
+const solution = (A, B) => (B + B).indexOf(A);
