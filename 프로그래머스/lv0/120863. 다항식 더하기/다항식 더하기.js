e758b8e8c5eb00d59cb6521c702cfabb9846580a
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-29 10:39:04 +0900

    [level 0] Title: 다항식 더하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,13 +1,13 @@
 function solution(polynomial) {
-    const arr = polynomial.split(' + ');
+    const arr = polynomial.split(" + ");
     const answer = arr.reduce((a, c) => {
-        if (c.indexOf('x') === -1) {
+        if (c.indexOf("x") === -1) {
             a[1] = (a[1] || 0) + +c;
         } else {
-            a[0] = (parseInt(a[0]) || 0) + (parseInt(c) || 1) + 'x';
+            a[0] = (parseInt(a[0]) || 0) + (parseInt(c) || 1) + "x";
         }
         return a;
     }, []);
-    parseInt(answer[0]) === 1 && (answer[0] = 'x');
-    return answer.flat(2).join(' + ');
+    parseInt(answer[0]) === 1 && (answer[0] = "x");
+    return answer.flat(2).join(" + ");
 }
